03475e19c5c9c22f029f7dd9610bb460aae04f1f
Surojit Mondal <gobindomondal539@gmail.com>
2025-01-09 11:38:12 +0530

    Time: 2 ms (63.31%) | Memory: 9.7 MB (46.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,24 +3,18 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.length();
         int m = t.length();
-        bool flag;
         vector<int> arr(26, 0);
         if (n != m) {
             return false;
-        } else {
-            for (int i = 0; i < n; i++) {
-                arr[s[i] - 'a'] = arr[s[i] - 'a'] + 1;
-                arr[t[i] - 'a'] = arr[t[i] - 'a'] - 1;
-            }
-            for (int j = 0; j < 26; j++) {
-                if (arr[j] != 0) {
-                    flag = false;
-                    break;
-                } else {
-                    flag = true;
-                }
-            }
         }
-        return flag;
+        for (int i = 0; i < n; i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+        for (int j = 0; j < 26; j++) {
+            if (arr[j] != 0)
+                return false;
+        }
+        return true;
     }
 };
